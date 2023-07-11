982955ce03405e5427b2d757e2e7b815d48d1cc3
Koushik Saha <93317898+Gituatu@users.noreply.github.com>
2023-07-11 21:05:55 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders= [
+    {
+        productName: 'Foldable mini drone',
+        productNumber: '793509',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Larvender kf057 drone',
+        productNumber: '76309',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Dono hd5 drone',
+        productNumber: '53359',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Ultra camon mini drone',
+        productNumber: '805509',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS Mi2 drone',
+        productNumber: '283488',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Vision 4k drone',
+        productNumber: '87875',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Water proof drone',
+        productNumber: '99754',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
